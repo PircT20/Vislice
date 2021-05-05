9a53d326965a61d96d66c79263305c8bb5cddcdb
Tea Pirc <tea.pirc@gmail.com>
2021-05-05 20:12:21 +0200

    ODPRAVA NAPAK (5. 05.)

--- model.py
+++ model.py
@@ -1,11 +1,16 @@
-STEVILO_DOVOLJENIH_NAPAK = 10
+STEVILO_DOVOLJENIH_NAPAK = 9
+
 PRAVILNA_CRKA, PONOVLJENA_CRKA, NAPACNA_CRKA = '+', 'o', '-'
 ZMAGA, PORAZ = 'W', 'X'
+ZACETEK = 'Z'
 
 class Igra:
-    def __init__(self, geslo, crke = []):
+    def __init__(self, geslo, crke = None):
         self.geslo = geslo
-        self.crke = crke
+        if crke is None:
+            self.crke = []
+        else:
+            self.crke = crke
 
     def napacne_crke(self):
         return [c for c in self.crke if c.upper() not in self.geslo.upper()]
@@ -61,3 +66,29 @@ import random
 def nova_igra():
     geslo = random.choice(bazen_besed)
     return Igra(geslo)
+
+
+class Vislice:
+
+    def __init__(self):
+        self.igre = {}
+
+    def prost_id_igre(self):
+        if len(self.igre) == 0:
+            return 0
+        else: 
+            return max(self.igre.keys()) + 1
+
+    def nova_igra(self):
+        id_igre = self.prost_id_igre()
+        igra = nova_igra()
+        self.igre[id_igre] = (igra, ZACETEK)
+        return id_igre
+
+    def ugibaj(self, id_igre, crka):
+        igra, _ = self.igre[id_igre]   #namesto stanja pišemo podčrtaj, saj je to spremenljivka, ki je ne potrebujemo
+        stanje = igra.ugibaj(crka)
+        self.igre[id_igre] = (igra, stanje)
+
+
+
